bd18b7ade66bce5e0965f6f6a231f13aa8cc7cb7
plushugh <www.linuxforum.net@gmail.com>
2019-01-19 11:15:44 +0800

    Update controler.ino

--- controler/controler.ino
+++ controler/controler.ino
@@ -7,31 +7,21 @@
 // changed for DHT22 (AM2302), then use random value instead real value sensor by niq_ro (Nicu FLORICA)
 // from http://nicuflorica.blogspot.ro 
  
-//#include <DHT.h>    // use just if use real sensor not for random
 #include <ESP8266WiFi.h>
  
 // replace with your channel's thingspeak API key, 
-String apiKey = "XJ61NMIT0942SEYS";
+String apiKey = "YQQALXB4VXS3V3Z5";
 const char* ssid = "Plus-2.4";
 const char* password = "love771221";
 const char* server = "api.thingspeak.com";
 
-// uncomment if use real sensor
-/*
-#define DHTPIN 5 // what pin we're connected to
-// note GPIO5 is D1 at NodeMCU - http://www.14core.com/wp-content/uploads/2015/06/Node-MCU-Pin-Out-Diagram1.png
-// note GPIO5 is D15 http://www.wemos.cc/wiki/doku.php?id=en:d1
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22 
-DHT dht(DHTPIN, DHTTYPE,11);
-*/
 WiFiClient client;
    
- 
+const int photoresistor = A0; // Photoresistor at Arduino analog pin A0
+int light = 0;
 void setup() {                
   Serial.begin(115200);
   delay(10);
-//  dht.begin();  // uncomment if use real sensor
   
   WiFi.begin(ssid, password);
  
@@ -53,31 +43,21 @@ void setup() {
  
  
 void loop() {
-// uncomment if use real sensor
-/*   
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-*/
-
-// comment if use real sensor
-float h = random(3000,5000);
-h = h / 100;
-  
-float t = random (2000,2500);
-t = t / 100;
-// end part of random values
-  
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+  int lightval = analogRead(photoresistor);
+  if (lightval >40) {
+    light = 1;
+    }else{
+      light =0;
+      }
+  postdata (light);
  
-  if (client.connect(server,80)) {  //   "184.106.153.149" or api.thingspeak.com
+  }
+
+void postdata (int light) {
+    if (client.connect(server,80)) {  //   "184.106.153.149" or api.thingspeak.com
     String postStr = apiKey;
            postStr +="&field1=";
-           postStr += String(t);
-           postStr +="&field2=";
-           postStr += String(h);
+           postStr += String(light);
            postStr += "\r\n\r\n";
  
      client.print("POST /update HTTP/1.1\n"); 
@@ -89,14 +69,8 @@ t = t / 100;
      client.print(postStr.length()); 
      client.print("\n\n"); 
      client.print(postStr);
-           
- 
-     Serial.print("Temperature: ");
-     Serial.print(t);
-     Serial.print(" degrees Celcius Humidity: "); 
-     Serial.print(h);
-     Serial.println("% send to Thingspeak");    
-  }
+    }
+  
   client.stop();
    
   Serial.println("Waiting...");    
