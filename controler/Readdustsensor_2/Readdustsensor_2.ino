1f612aaeecdd5b1cb5cf952a92b37c59686d12e3
plushugh <www.linuxforum.net@gmail.com>
2019-01-27 09:44:43 +0800

    New gen of dust sensor reader

---
+++ controler/Readdustsensor_2/Readdustsensor_2.ino
@@ -0,0 +1,115 @@
+/*********************************************************************************************************
+*
+* File                : DustSensor
+* Hardware Environment: 
+* Build Environment   : Arduino
+* Version             : V1.0.5-r2
+* By                  : WaveShare
+*
+*                                  (c) Copyright 2005-2011, WaveShare
+*                                       http://www.waveshare.net
+*                                       http://www.waveshare.com   
+*                                          All Rights Reserved
+*
+*********************************************************************************************************/
+#define        COV_RATIO                       0.2            //ug/mmm / mv
+#define        NO_DUST_VOLTAGE                 400            //mv
+#define        SYS_VOLTAGE                     5000           
+
+
+/*
+I/O define
+*/
+const int iled = D0;                                            //drive the led of sensor
+const int vout = A0;                                            //analog input
+
+/*
+variable
+*/
+float density, voltage;
+int   adcvalue;
+
+/*
+private function
+*/
+int Filter(int m)
+{
+  static int flag_first = 0, _buff[10], sum;
+  const int _buff_max = 10;
+  int i;
+  
+  if(flag_first == 0)
+  {
+    flag_first = 1;
+
+    for(i = 0, sum = 0; i < _buff_max; i++)
+    {
+      _buff[i] = m;
+      sum += _buff[i];
+    }
+    return m;
+  }
+  else
+  {
+    sum -= _buff[0];
+    for(i = 0; i < (_buff_max - 1); i++)
+    {
+      _buff[i] = _buff[i + 1];
+    }
+    _buff[9] = m;
+    sum += _buff[9];
+    
+    i = sum / 10.0;
+    return i;
+  }
+}
+
+
+void setup(void)
+{
+  pinMode(iled, OUTPUT);
+  digitalWrite(iled, LOW);                                     //iled default closed
+  
+  Serial.begin(9600);                                         //send and receive at 9600 baud
+  
+}
+
+void loop(void)
+{
+  /*
+  get adcvalue
+  */
+  digitalWrite(iled, HIGH);
+  delayMicroseconds(280);
+  adcvalue = analogRead(vout);
+  digitalWrite(iled, LOW);
+  
+ // adcvalue = Filter(adcvalue);
+  
+  /*
+  covert voltage (mv)
+  */
+  voltage = (SYS_VOLTAGE / 1024.0) * adcvalue * 11;
+  
+  /*
+  voltage to density
+  */
+  if(voltage >= NO_DUST_VOLTAGE)
+  {
+    voltage -= NO_DUST_VOLTAGE;
+    
+    density = voltage * COV_RATIO;
+  }
+  else
+    density = 0;
+    
+  /*
+  display the result
+  */
+
+   Serial.print("RAW Analog: "+String(adcvalue)+" miliVOLT: "+String(voltage)+" DUST ug/m3: "+String(density)+"\n");
+
+ 
+  
+  delay(1000);
+}
