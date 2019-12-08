f3cb69b2e24ec485b6bad9ad0f908ca857db245a
plushugh <www.linuxforum.net@gmail.com>
2019-12-08 10:25:36 +0800

    Finished UI

--- DogCar/UserInterface/app.py
+++ DogCar/UserInterface/app.py
@@ -1,18 +1,31 @@
 import ctrlui
-from  PyQt5 import QtWidgets as qtw
+from PyQt5 import QtWidgets as qtw
 
 class Form(qtw.QMainWindow, ctrlui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.setupUi(self)
 
-def show():
+def fwd():
     print("Foward")
-
+def bak():
+    print("Backward")
+def left():
+    print("Turn Left")
+def right():
+    print("Turn Right")
 def main():
     app = qtw.QApplication([])
     window = Form()
-    window.fowardButton.clicked.connect(show)
+    window.fowardButton.clicked.connect(fwd)
+    window.backwardButton.clicked.connect(bak)
+    window.leftButton.clicked.connect(left)
+    window.rightButton.clicked.connect(right)
+    window.horizontalSlider.setTickPosition(qtw.QSlider.TicksBelow)
+    def printSpeedValue():
+        speedValue = window.horizontalSlider.value()
+        print(speedValue)
+    window.horizontalSlider.valueChanged.connect(printSpeedValue)
     window.show()
     app.exec_()
 if __name__ == "__main__":
