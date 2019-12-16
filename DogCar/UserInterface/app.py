3443cb3f9ed886c76a7c1e9e65ca59d858e299e5
plushugh <hughhji@outlook.com>
2019-12-16 17:58:43 +0800

    Update app.py

--- DogCar/UserInterface/app.py
+++ DogCar/UserInterface/app.py
@@ -2,27 +2,59 @@ import ctrlui
 import RPi.GPIO as GPIO
 import time
 from PyQt5 import QtWidgets as qtw
-ENA =
-ENB = 
-MOTOR1A = 
-MOTOR1B =
-MOTOR2A =
-MOTOR2A =
+ENA = 12
+ENB = 33
+MOTOR1A = 15 #LEFT
+MOTOR1B = 16 #LEFT
+MOTOR2A = 18 #RIGHT
+MOTOR2B = 22 #RIGHT
 class Form(qtw.QMainWindow, ctrlui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.setupUi(self)
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup()
+    GPIO.setup(MOTOR1A,GPIO.OUT)
+    GPIO.setup(MOTOR1B,GPIO.OUT)
+    GPIO.setup(MOTOR2A,GPIO.OUT)
+    GPIO.setup(MOTOR2B,GPIO.OUT)
+    GPIO.setup(ENA,GPIO.OUT)
+    GPIO.setup(ENB,GPIO.OUT)
+    GPIO.output(MOTOR1A,GPIO.LOW)
+    GPIO.output(MOTOR1B,GPIO.LOW)
+    GPIO.output(MOTOR2A,GPIO.LOW)
+    GPIO.output(MOTOR2B,GPIO.LOW)
+    en1=GPIO.PWM(ENA,1000)
+    en2=GPIO.PWM(ENB,1000)
+    en1.start(0)
+    en2.start(0)
+
+def changeSpeed(spdVal):
+    en1.change
 
 def fwd():
+    GPIO.output(MOTOR1A,GPIO.HIGH)
+    GPIO.output(MOTOR1B,GPIO.LOW)
+    GPIO.output(MOTOR2A,GPIO.HIGH)
+    GPIO.output(MOTOR2B,GPIO.LOW)
     print("Forward")
 def bak():
+    GPIO.output(MOTOR1A,GPIO.LOW)
+    GPIO.output(MOTOR1B,GPIO.HIGH)
+    GPIO.output(MOTOR2A,GPIO.LOW)
+    GPIO.output(MOTOR2B,GPIO.HIGH)
     print("Backward")
 def left():
-    print("Turn Left")
+    GPIO.output(MOTOR1A,GPIO.LOW)
+    GPIO.output(MOTOR1B,GPIO.HIGH)
+    GPIO.output(MOTOR2A,GPIO.HIGH)
+    GPIO.output(MOTOR2B,GPIO.LOW)
+    print("Turn Right")
 def right():
+    GPIO.output(MOTOR1A,GPIO.HIGH)
+    GPIO.output(MOTOR1B,GPIO.LOW)
+    GPIO.output(MOTOR2A,GPIO.LOW)
+    GPIO.output(MOTOR2B,GPIO.HIGH)
     print("Turn Right")
 def main():
     app = qtw.QApplication([])
